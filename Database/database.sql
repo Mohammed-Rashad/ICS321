22777ff4b85ad67d80a0c7747790179784b57879
Marwanshtine <eng.mrwanm@gmail.com>
2024-12-13 15:56:28 +0300

    Added phone, email, password to passenger

--- Database/database.sql
+++ Database/database.sql
@@ -52,6 +52,9 @@ CREATE TABLE `passenger` (
   `ID` int NOT NULL,
   `Name` varchar(15) NOT NULL,
   `Balance` int NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` varchar(14) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
